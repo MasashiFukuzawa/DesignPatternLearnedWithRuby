20d0711aebd59d97de3c84a24427c96a66ab204d
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2019-12-10 21:36:33 +0900

    tweak variable names

--- chap03-template-method/main.rb
+++ chap03-template-method/main.rb
@@ -2,8 +2,8 @@ require './abstract_display'
 require './char_display'
 require './string_display'
 
-d1 = CharDisplay.new('H')
-d2 = StringDisplay.new('Hello, world.')
+char_display   = CharDisplay.new('H')
+string_display = StringDisplay.new('Hello, world.')
 
-d1.display
-d2.display
+char_display.display
+string_display.display
