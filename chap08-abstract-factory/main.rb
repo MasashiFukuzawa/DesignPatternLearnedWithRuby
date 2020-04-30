b1f28fecd9679b57006e4ebedbd670251a498b8d
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-04-30 23:46:06 +0900

    convert Japanese to English

--- chap08-abstract-factory/main.rb
+++ chap08-abstract-factory/main.rb
@@ -16,15 +16,15 @@ class Main
   def main(classname, filename)
     factory = Factory.new.factory(classname)
 
-    asashi = factory.create_link('朝日新聞', 'https://www.asashi.com')
-    yomiuri = factory.create_link('読売新聞', 'https://www.yomiuri.co.jp')
+    asashi = factory.create_link('Asahi', 'https://www.asashi.com')
+    yomiuri = factory.create_link('Yomiuri', 'https://www.yomiuri.co.jp')
 
     us_yahoo = factory.create_link('Yahoo!', 'https://www.yahoo.com')
     jp_yahoo = factory.create_link('Yahoo!Japan', 'https://www/yahoo.co.jp')
     excite = factory.create_link('Excite', 'https://www.excite.com')
     google = factory.create_link('Google', 'https://www.google.com')
 
-    traynews = factory.create_tray('新聞')
+    traynews = factory.create_tray('Newspaper')
     traynews.add(us_yahoo)
     traynews.add(yomiuri)
 
@@ -32,12 +32,12 @@ class Main
     trayyahoo.add(us_yahoo)
     trayyahoo.add(jp_yahoo)
 
-    traysearch = factory.create_tray('サーチエンジン')
+    traysearch = factory.create_tray('Search Engine')
     traysearch.add(trayyahoo)
     traysearch.add(excite)
     traysearch.add(google)
 
-    page = factory.create_page(filename, '鈴木 一郎')
+    page = factory.create_page(filename, 'Ichiro Suzuki')
     page.add(traynews)
     page.add(traysearch)
     page.output
